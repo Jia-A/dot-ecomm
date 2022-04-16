ce82d9aaf07186412ebd1f2f8f83761bc17fe464
Jia-A <agrawaljiya2011@gmail.com>
2022-04-16 19:52:04 +0530

    login-signup-added

--- src/dot-components/Signup/signup.jsx
+++ src/dot-components/Signup/signup.jsx
@@ -1,40 +1,13 @@
-import "../../public-css/navbar.css";
 import "./signup.css"
 import "../../public-css/root.css";
 import "../../public-css/sign-login-auth.css"
+import { Link } from "react-router-dom";
+import { Navbar } from "../Navbar/navbar";
 
 const Signup = () =>{
     return (
       <div className="App">
-        <div class="nav-con">
-        <nav class="nav-bar">
-            <div>
-                <h2 class="website-name"><a href="../../index.html" class="link-style link-color-primary"> D<img src="./images/dot.png" class="dot-img"/>T Store</a></h2>
-
-                <p class="sm-txt"><i>Everything ends with a dot.</i></p>
-            </div>
-            
-            <div class="box">
-                <input type="text" placeholder="Search" class="search-box input"/>
-            </div> 
-            <div class="nav-btn">
-                <button class="btn secondary-btn"><a href="../login/login.html" class="link-style link-color">Login</a></button>
-                <div class="btn-badge">
-                    <button class="btn icon-only-btn"><i class="far fa-user icon"></i></button>
-                    <div class="real-badge">6</div>
-                </div>
-                <div class="btn-badge">
-                    <button class="btn icon-only-btn"><a href="../wishlist/wishlist.html"><i class="far fa-heart icon"></i></a></button>
-                    <div class="real-badge">4</div>
-                </div>
-                
-                <div class="btn-badge">
-                    <button class="btn icon-only-btn"><a href="../cart/cart.html"><i class="far fa-shopping-cart icon"></i></a></button>
-                    <div class="real-badge">1</div>
-                </div>
-            </div>
-        </nav>
-    </div>
+        <Navbar/>
     <main class="form-main align-justify-center margin-30">
        
         <div class="form-container align-justify-center">
@@ -51,12 +24,16 @@ const Signup = () =>{
                 <input type="password" class="input" id="password" required/>
                     <label for="">
                         <input type="checkbox" id="t-c" required/>
-                            I accept all the terms and conditions
+                            I accept all the terms and conditions.
                     </label>
                 <div>
-                    <button class="submit-btn btn primary-btn">Signup</button>
+                    <Link to="/">
+                        <button class="submit-btn btn primary-btn">Signup</button>
+                    </Link>
                 </div>
-                <button class="new-ac button read-btn"><a href="../login/login.html" class="link-style link-color-primary"> Already have an account</a></button>
+                <Link to="/login" className="link-style link-color-primary">
+                    <button class="new-ac button read-btn"> Already have an account</button>
+                </Link>
             </form>
         </div>
     </main>
